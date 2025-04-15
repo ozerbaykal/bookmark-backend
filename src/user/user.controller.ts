b2a86a676950ef0157c15377732fec770c13fd76
ozerbaykal <baykalozer87@gmail.com>
2025-04-16 01:36:26 +0300

    add getUsers and UpdateUsers userservise and usercontroller both

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch } from '@nestjs/common';
+import { UserService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/profile')
+  getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @Patch('/update')
+  updateUser(@Body() body: any) {
+    return this.userService.updateUser(body);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUsers() {
+    return this.prisma.user.findMany();
+  }
+
+  async updateUser(body: any) {
+    return this.prisma.user.update({
+      where: { id: body.id },
+      data: body,
+    });
+  }
+}
