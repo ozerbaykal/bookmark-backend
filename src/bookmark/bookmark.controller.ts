9238ceca0f0d4aef9e70afd96ec841665bb3bd7b
ozerbaykal <baykalozer87@gmail.com>
2025-04-16 11:43:10 +0300

    add getbookmarks and singlebookmark methods

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,28 +1,50 @@
+import { Bookmark } from './../../node_modules/.prisma/client/index.d';
 import {
   Body,
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { BookmarkService } from './bookmark.service';
 import { GetUser } from 'src/auth/decorator';
 import { CreateBookmarkDto } from './dto/create-bookmark.dto';
 import { EditBookmarkDto } from './dto/edit-bookmark.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('bookmark')
 export class BookmarkController {
   constructor(private bookmarkService: BookmarkService) {}
+  @UseGuards(AuthGuard('jwt'))
   @Get()
-  getBookmarks(@GetUser('id') userId: number) {
-    return this.bookmarkService.getBookmarks(userId);
+  async getBookmarks(@GetUser('id') userId: number) {
+    const bookmarks = await this.bookmarkService.getBookmarks(userId);
+    if (!bookmarks || bookmarks.length === 0) {
+      throw new NotFoundException('No bookmarks found');
+    }
+    return bookmarks;
   }
+  //belirli bir bookmark'ı getirmek için
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
-  getBookmarkById(@Param('id') bookmarkId: string) {
-    return this.bookmarkService.getBookmarkById(bookmarkId);
+  async getBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id') bookmarkId: string,
+  ) {
+    const bookmark = await this.bookmarkService.getBookmarkById(
+      userId,
+      bookmarkId,
+    );
+    if (!bookmark) {
+      throw new NotFoundException('Bookmark not found');
+    }
+    return bookmark;
   }
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   createBookmark(
     @GetUser('id') userId: number,
@@ -30,6 +52,7 @@ export class BookmarkController {
   ) {
     return this.bookmarkService.createBookmark(userId, body);
   }
+  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
   updateBookmark(
     @GetUser('id') userId: number,
@@ -38,6 +61,7 @@ export class BookmarkController {
   ) {
     return this.bookmarkService.updateBookmark(userId, bookmarkId, body);
   }
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   deleteBookmark(
     @GetUser('id') userId: number,
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -6,9 +6,17 @@ import { CreateBookmarkDto } from './dto/create-bookmark.dto';
 export class BookmarkService {
   constructor(private prisma: PrismaService) {}
   getBookmarks(userId: number) {
-    return this.prisma.bookmark.findMany();
+    return this.prisma.bookmark.findMany({
+      where: {
+        userId,
+      },
+    });
   }
-  getBookmarkById(bookmarkId: string) {}
+  getBookmarkById(userId: number, bookmarkId: string) {
+    return this.prisma.bookmark.findUnique({
+      where: { id: parseInt(bookmarkId), userId },
+    });
+  }
   createBookmark(userId: number, body: CreateBookmarkDto) {}
   updateBookmark(userId: number, bookmarkId: string, body: EditBookmarkDto) {}
   deleteBookmark(userId: number, bookmarkId: string) {}
